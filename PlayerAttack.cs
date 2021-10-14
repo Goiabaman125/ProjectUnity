22b52818932dab5af7c85f0da6e95ed9dd9cfc5f
Goiabaman125 <90534446+Goiabaman125@users.noreply.github.com>
2021-10-14 09:50:32 -0300

    Add files via upload

---
+++ PlayerAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour{
+
+ private float timebtwAttack;
+ public float starttimebtwAttack;
+ public Transform attackPos;
+ public LayerMask whatIsEnemies;
+ public float attackRange;
+ public int damage;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timebtwAttack <=0)
+       if(Input.GetKey(KeyCode.Space)) 
+       Collider2D[] enemiestodamage= Physics2D.OverlapCircleAll(attackPos.position,attackRange,whatIsEnemies);
+       for (int i=o; i < enemiestodamage.Lenght;i++){
+           enemiestodamage[i].GetComponent<Enemy>().TakeDamage(damage);
+       }
+    
+    timebtwAttack = starttimebtwAttack;
+    else
+    {
+        timebtwAttack -= Time.deltaTime;
+    }
+
+    }
+    void OnDrawGizmosSelected(){
+        Gizmos.color=Color.red;
+        Gizmos.DrawWireSphere(attackPos.position,attackRange);
+    }
+}
